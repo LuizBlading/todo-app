d89b368c6aed058e951a42429afc7393a62ee031
LuizBlading <luizfelipe1342@yahoo.com.br>
2021-08-19 02:32:51 -0300

    Edit Divs | Styles | Add Buttons

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 let inputTask = document.querySelector('#inputTODO');
 let result = document.querySelector('.listTODO');
+let container = document.querySelector('.container');
 let btnCompleteTask;
+let p;
 
 inputTask.value = ''
 inputTask.addEventListener("keyup", getInputValue);
+window.addEventListener('load', drawButton);
 
 function getInputValue(e){
     if(e.keyCode === 13)
@@ -17,17 +20,23 @@ function getInputValue(e){
 
 function addTask(valueTask){
     btnCompleteTask = document.createElement('button');
-    let p = document.createElement('p');
+    p = document.createElement('p');
     let value = document.createTextNode(valueTask);
     let div = document.createElement('div');
+    let divButton = document.createElement('div');
+    let divTextTask = document.createElement('div');
 
-    btnCompleteTask.setAttribute('class', 'btnComplete')
     div.setAttribute('class', 'taskComponent');
+    divButton.setAttribute('class', 'divButton');
+    divTextTask.setAttribute('class', 'divTextTask');
+    btnCompleteTask.setAttribute('class', 'btnComplete');
 
     div.style.wordBreak = "break-all";
     p.appendChild(value);
-    div.appendChild(btnCompleteTask);
-    div.appendChild(p);
+    divTextTask.appendChild(p);
+    divButton.appendChild(btnCompleteTask);
+    div.appendChild(divButton);
+    div.appendChild(divTextTask);
 
     result.appendChild(div);
 
@@ -37,4 +46,30 @@ function addTask(valueTask){
 
 function completeTask(){
     btnCompleteTask.setAttribute('class', 'complete');
+    p.style.textDecoration = "line-through";
+}
+
+function drawButton(){
+    let divButtons = document.createElement('div');
+    let divButtonCompleteAll = document.createElement('div');
+    let divButtonClearAll = document.createElement('div');
+
+    let buttonCompleteAll = document.createElement('button');
+    let buttonClearAll = document.createElement('button');
+
+    let txtBtnComplete = document.createTextNode('Completar Todos');
+    let txtBtnClear = document.createTextNode('Limpar');
+
+    // criar atributos (classes)
+
+    buttonCompleteAll.appendChild(txtBtnComplete);
+    buttonClearAll.appendChild(txtBtnClear);
+
+    divButtonCompleteAll.appendChild(buttonCompleteAll);
+    divButtonClearAll.appendChild(buttonClearAll);
+
+    divButtons.appendChild(divButtonCompleteAll);
+    divButtons.appendChild(divButtonClearAll);
+
+    container.appendChild(divButtons);
 }
